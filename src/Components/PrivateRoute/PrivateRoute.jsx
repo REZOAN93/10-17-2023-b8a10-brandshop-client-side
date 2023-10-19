50dfffe997d91b45985ab1b9a6e00696dcb1d2fa
REZOAN93 <rezoanshawonweb@gmail.com>
2023-10-20 03:29:41 +0600

    Updated the private route reload icon

--- src/Components/PrivateRoute/PrivateRoute.jsx
+++ src/Components/PrivateRoute/PrivateRoute.jsx
@@ -6,12 +6,19 @@ const PrivateRoute = ({ children }) => {
   const location = useLocation();
   const { user, loading } = useContext(AuthContext);
   if (loading) {
-    return <div>Loading....</div>;
+    return (
+      <div className="py-32 w-full flex justify-center">
+        <span className="loading loading-dots loading-xs"></span>
+        <span className="loading loading-dots loading-sm"></span>
+        <span className="loading loading-dots loading-md"></span>
+        <span className="loading loading-dots loading-lg"></span>
+      </div>
+    );
   }
   if (user) {
     return children;
   }
-  
+
   return <Navigate to={"/signin"} state={location.pathname}></Navigate>;
 };
 
